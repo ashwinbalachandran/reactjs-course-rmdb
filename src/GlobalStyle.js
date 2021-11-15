15be17af96859d74543e028d11c1f753638ac487
Ashwin Balachandran <ash20win@gmail.com>
2021-11-14 23:28:37 -0800

    Broke everything

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -21,7 +21,8 @@ export const GlobalStyle = createGlobalStyle`
     body {
         margin:0;
         padding:0;
-
+        box-sizing: border-box;
+        
         h1 {
             font-size: 2rem;
             font-weight: 600;
