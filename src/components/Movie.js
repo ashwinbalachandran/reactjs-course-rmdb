e8dddccf44d4b49f03555e1573c052619e420501
Ashwin Balachandran <ash20win@gmail.com>
2021-11-28 11:31:38 -0800

    Movie Info part 1 completed

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 // Config
-import { IMAGE_BASE_URL, PSOTER_SIZE } from '../config'
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config'
 // Components
 import Grid from './Grid'
 import Spinner from './Spinner'
--- src/components/MovieInfo/index.js
+++ src/components/MovieInfo/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 // Components
 import Thumb from '../Thumb'
 // Config
-import { Image_BASE_URL, POSTER_SIZE } from '../../config'
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../../config'
 // Image
 import NoImage from '../../images/no_image.jpg'
 // Styles
@@ -14,11 +14,30 @@ const MovieInfo = ({ movie }) => (
             <Thumb
                 image={
                     movie.poster_path
-                        ? `${Image_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+                        ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
                         : NoImage
                 }
                 clickable={false}
             />
+            <Text>
+                <h1>{movie.title}</h1>
+                <h3>PLOT SUMMARY</h3>
+                <p>{movie.overview}</p>
+                <div className="rating-directors">
+                    <div>
+                        <h3>RATING</h3>
+                        <div className="score">{movie.vote_average}</div>
+                        <div className="director">
+                            <h3>
+                                DIRECTOR{movie.directors.length > 1 ? 'S' : ''}
+                            </h3>
+                            {movie.directors.map((director) => (
+                                <p key={director.credit_id}>{director.name}</p>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </Text>
         </Content>
     </Wrapper>
 )
